f051514502cdb39195dd7c640790719d8072fc76
Alex Hladun <alexhladun@gmail.com>
2021-05-18 20:48:24 -0600

    improve styles

--- assets/styles/GameIndicator.js
+++ assets/styles/GameIndicator.js
@@ -9,32 +9,26 @@ export const styles = StyleSheet.create({
     position: "absolute",
     justifyContent: "center",
     alignItems: "center",
-    bottom: 100,
-    right: 17,
+    bottom: 90,
+    right: 16,
     opacity: 0.8
   },
   screenContainer: {
     padding: 10,
     justifyContent: "center",
     alignItems: "center",
-    // bottom: 130,
-    // left: 50,
     alignSelf: "center",
     backgroundColor: Theme.spinGreen1,
-    // width: "85%",
-    // height: "50%",
     borderRadius: 20
   },
   chip: {
     zIndex: 201,
-    // position: "absolute",
-    width: 75,
-    height: 75,
+    width: 70,
+    height: 70,
     borderRadius: 100,
-    backgroundColor: "#9FD983",
+    backgroundColor: Theme.spinGreen3,
     justifyContent: "center",
     alignItems: "center"
-    // overflow: "hidden"
   },
   holeContainer: {
     flexDirection: "column",
